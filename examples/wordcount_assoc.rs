c3567b968192b799699f36ece45571d30f05fa89
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-03-25 20:52:13 +0100

    example: wordcount associative

---
+++ examples/wordcount_assoc.rs
@@ -0,0 +1,62 @@
+use std::time::Instant;
+
+use regex::Regex;
+
+use rstream::block::BatchMode;
+use rstream::config::EnvironmentConfig;
+use rstream::environment::StreamEnvironment;
+use rstream::operator::source;
+
+fn main() {
+    let (config, args) = EnvironmentConfig::from_args();
+    if args.len() != 1 {
+        panic!("Pass the dataset path as an argument");
+    }
+    let path = &args[0];
+
+    let mut env = StreamEnvironment::new(config);
+
+    env.spawn_remote_workers();
+
+    let source = source::FileSource::new(path);
+    let tokenizer = Tokenizer::new();
+    let stream = env
+        .stream(source)
+        .batch_mode(BatchMode::fixed(1024))
+        .flat_map(move |line| tokenizer.tokenize(line))
+        .group_by_fold(
+            |word| word.clone(),
+            0,
+            |count, _word| count + 1,
+            |a, b| a + b,
+        )
+        .unkey();
+    let result = stream.collect_vec();
+    let start = Instant::now();
+    env.execute();
+    let elapsed = start.elapsed();
+    if let Some(res) = result.get() {
+        eprintln!("Output: {:?}", res.len());
+    }
+    eprintln!("Elapsed: {:?}", elapsed);
+}
+
+struct Tokenizer {
+    re: Regex,
+}
+
+impl Tokenizer {
+    fn new() -> Self {
+        Self {
+            re: Regex::new(r"[^A-Za-z]+").unwrap(),
+        }
+    }
+    fn tokenize(&self, value: String) -> Vec<String> {
+        self.re
+            .replace_all(&value, " ")
+            .split_ascii_whitespace()
+            .filter(|word| !word.is_empty())
+            .map(|t| t.to_lowercase())
+            .collect()
+    }
+}
