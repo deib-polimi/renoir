51b41f8d6b928467bc9be16cbda57a3475ab0438
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-04-05 18:16:31 +0200

    for_each sink for KeyedStream

--- src/operator/sink/for_each.rs
+++ src/operator/sink/for_each.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
 
 use crate::operator::sink::Sink;
-use crate::operator::{Data, Operator, StreamElement};
+use crate::operator::{Data, DataKey, Operator, StreamElement};
 use crate::scheduler::ExecutionMetadata;
-use crate::stream::Stream;
+use crate::stream::{KeyValue, KeyedStream, Stream};
 
 #[derive(Clone, Derivative)]
 #[derivative(Debug)]
@@ -59,6 +59,15 @@ where
     }
 }
 
+impl<Key: DataKey, Out: Data, OperatorChain> KeyedStream<Key, Out, OperatorChain>
+where
+    OperatorChain: Operator<KeyValue<Key, Out>> + Send + 'static,
+{
+    pub fn for_each<F: Fn(Key, Out) + Send + Sync + 'static>(self, f: F) {
+        self.0.for_each(move |(key, out)| f(key, out))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::sync::atomic::{AtomicU8, Ordering};
@@ -79,5 +88,23 @@ mod tests {
         });
         env.execute();
         assert_eq!(sum2.load(Ordering::Acquire), (0..10).sum::<u8>());
+    }
+
+    #[test]
+    fn for_each_keyed() {
+        let mut env = StreamEnvironment::new(EnvironmentConfig::local(4));
+        let source = source::IteratorSource::new(0..10u8);
+        let sum = Arc::new(AtomicU8::new(0));
+        let sum2 = sum.clone();
+        env.stream(source)
+            .group_by(|x| x % 2)
+            .for_each(move |p, x| {
+                sum.fetch_add(x * (p + 1), Ordering::Release);
+            });
+        env.execute();
+        assert_eq!(
+            sum2.load(Ordering::Acquire),
+            (0..10).map(|x| x * (x % 2 + 1)).sum::<u8>()
+        );
     }
 }
