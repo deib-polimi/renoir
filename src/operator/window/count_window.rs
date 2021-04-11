f470b6b82c69f1c324c08539d4e8bc3a2d9eeb66
Marco Donadoni <marcodonadoni@live.it>
2021-04-11 18:32:02 +0200

    Make count windows handle timestamps correctly

--- src/operator/window/count_window.rs
+++ src/operator/window/count_window.rs
@@ -43,6 +43,7 @@ pub struct CountWindowGenerator<Key: DataKey, Out: Data> {
     buffer: VecDeque<Out>,
     timestamp_buffer: VecDeque<Timestamp>,
     received_end: bool,
+    last_watermark: Option<Timestamp>,
     _key: PhantomData<Key>,
 }
 
@@ -53,6 +54,7 @@ impl<Key: DataKey, Out: Data> CountWindowGenerator<Key, Out> {
             buffer: Default::default(),
             timestamp_buffer: Default::default(),
             received_end: false,
+            last_watermark: None,
             _key: Default::default(),
         }
     }
@@ -66,9 +68,7 @@ impl<Key: DataKey, Out: Data> WindowGenerator<Key, Out> for CountWindowGenerator
                 self.buffer.push_back(item);
                 self.timestamp_buffer.push_back(ts);
             }
-            StreamElement::Watermark(_) => {
-                // Watermarks are not used by count windows
-            }
+            StreamElement::Watermark(ts) => self.last_watermark = Some(ts),
             StreamElement::FlushBatch => unreachable!("Windows do not handle FlushBatch"),
             StreamElement::End => {
                 self.received_end = true;
@@ -80,12 +80,21 @@ impl<Key: DataKey, Out: Data> WindowGenerator<Key, Out> for CountWindowGenerator
         if self.buffer.len() >= self.descr.size.get()
             || (self.received_end && !self.buffer.is_empty())
         {
+            let size = self.descr.size.get().min(self.buffer.len());
+            let timestamp_items = self.timestamp_buffer.iter().take(size).max().cloned();
+            let timestamp = match &(timestamp_items, self.last_watermark) {
+                (Some(ts), Some(w)) => {
+                    // Make sure timestamp is correct with respect to watermarks
+                    Some((*ts).max(*w + Timestamp::from_nanos(1)))
+                }
+                (Some(ts), _) => Some(*ts),
+                _ => None,
+            };
+
             Some(Window {
-                size: self.descr.size.get().min(self.buffer.len()),
+                size,
                 gen: self,
-                // TODO: handle timestamp
-                // TODO: make sure the last incomplete windows do not have timestamps bigger than old watermarks
-                timestamp: None,
+                timestamp,
             })
         } else {
             None
@@ -101,5 +110,46 @@ impl<Key: DataKey, Out: Data> WindowGenerator<Key, Out> for CountWindowGenerator
 
     fn buffer(&self) -> &VecDeque<Out> {
         &self.buffer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::operator::{CountWindow, StreamElement, WindowDescription, WindowGenerator};
+    use std::time::Duration;
+
+    #[test]
+    fn count_window_watermark() {
+        let descr = CountWindow::new(3, 2);
+        let mut generator = descr.new_generator();
+
+        generator.add(StreamElement::Timestamped((0, 1), Duration::from_secs(1)));
+        assert!(generator.next_window().is_none());
+        generator.add(StreamElement::Timestamped((0, 2), Duration::from_secs(2)));
+        assert!(generator.next_window().is_none());
+        generator.add(StreamElement::Watermark(Duration::from_secs(4)));
+        assert!(generator.next_window().is_none());
+        generator.add(StreamElement::End);
+        let window = generator.next_window().unwrap();
+        assert_eq!(
+            window.timestamp,
+            Some(Duration::from_nanos(1) + Duration::from_secs(4))
+        );
+        assert_eq!(window.size, 2);
+    }
+
+    #[test]
+    fn count_window_timestamp() {
+        let descr = CountWindow::new(3, 2);
+        let mut generator = descr.new_generator();
+
+        generator.add(StreamElement::Timestamped((0, 1), Duration::from_secs(1)));
+        assert!(generator.next_window().is_none());
+        generator.add(StreamElement::Timestamped((0, 2), Duration::from_secs(2)));
+        assert!(generator.next_window().is_none());
+        generator.add(StreamElement::Timestamped((0, 3), Duration::from_secs(3)));
+        let window = generator.next_window().unwrap();
+        assert_eq!(window.timestamp, Some(Duration::from_secs(3)));
+        assert_eq!(window.size, 3);
     }
 }
