2568bd6e88785770467cee1820b083311f69a19d
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-04-13 09:40:45 +0200

    Function for getting the coordinates of the block the worker is processing

--- src/worker/mod.rs
+++ src/worker/mod.rs
@@ -1,7 +1,25 @@
+use std::cell::RefCell;
+
 use crate::block::InnerBlock;
 use crate::channel::BoundedChannelReceiver;
+use crate::network::Coord;
 use crate::operator::{Data, Operator, StreamElement};
 use crate::scheduler::{ExecutionMetadata, StartHandle};
+
+thread_local! {
+    /// Coordinates of the replica the current worker thread is working on.
+    ///
+    /// Access to this by calling `replica_coord()`.
+    static COORD: RefCell<Option<Coord>> = RefCell::new(None);
+}
+
+/// Get the coord of the replica the current thread is working on.
+///
+/// This will return `Some(coord)` only when called from a worker thread of a replica, otherwise
+/// `None` is returned.
+pub fn replica_coord() -> Option<Coord> {
+    COORD.with(|x| *x.borrow())
+}
 
 pub(crate) fn spawn_worker<Out: Data, OperatorChain>(
     block: InnerBlock<Out, OperatorChain>,
@@ -30,6 +48,8 @@ fn worker<Out: Data, OperatorChain>(
         metadata.coord,
         block.to_string(),
     );
+    // remember in the thread-local the coordinate of this block
+    COORD.with(|x| *x.borrow_mut() = Some(metadata.coord));
     // notify the operators that we are about to start
     block.operators.setup(metadata.clone());
     while !matches!(block.operators.next(), StreamElement::End) {
