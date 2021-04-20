424dab70ab48c56d2a1381653d2cbc24e967f19c
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-04-20 11:39:14 +0200

    Integration tests for Reduce

---
+++ tests/reduce.rs
@@ -0,0 +1,65 @@
+use rstream::operator::source::IteratorSource;
+use rstream::test::TestHelper;
+
+#[test]
+fn reduce_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u8);
+        let res = env.stream(source).reduce(|acc, v| acc + v).collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            assert_eq!(res.len(), 1);
+            assert_eq!(res[0], 45);
+        }
+    });
+}
+
+#[test]
+fn reduce_assoc_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u8);
+        let res = env
+            .stream(source)
+            .reduce_assoc(|acc, v| acc + v)
+            .collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            assert_eq!(res.len(), 1);
+            assert_eq!(res[0], 45);
+        }
+    });
+}
+
+#[test]
+fn reduce_shuffled_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u8);
+        let res = env
+            .stream(source)
+            .shuffle()
+            .reduce(|acc, v| acc + v)
+            .collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            assert_eq!(res.len(), 1);
+            assert_eq!(res[0], 45);
+        }
+    });
+}
+
+#[test]
+fn reduce_assoc_shuffled_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u8);
+        let res = env
+            .stream(source)
+            .shuffle()
+            .reduce_assoc(|acc, v| acc + v)
+            .collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            assert_eq!(res.len(), 1);
+            assert_eq!(res[0], 45);
+        }
+    });
+}
