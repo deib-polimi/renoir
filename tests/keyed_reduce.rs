a675bab022c8c8467f9df57e0830fa1bf4d77fe2
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-04-20 11:42:09 +0200

    Integration tests for KeyedReduce

---
+++ tests/keyed_reduce.rs
@@ -0,0 +1,45 @@
+use itertools::Itertools;
+
+use rstream::operator::source::IteratorSource;
+use rstream::test::TestHelper;
+
+#[test]
+fn group_by_reduce_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u32);
+        let res = env
+            .stream(source)
+            .map(|x| x.to_string())
+            .group_by_reduce(|n| n.parse::<u32>().unwrap() % 2, |acc, y| acc + &y)
+            .unkey()
+            .collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            let res = res.into_iter().sorted().collect_vec();
+            assert_eq!(res.len(), 2);
+            assert_eq!(res[0], (0, "02468".into()));
+            assert_eq!(res[1], (1, "13579".into()));
+        }
+    });
+}
+
+#[test]
+fn reduce_keyed_stream() {
+    TestHelper::local_remote_env(|mut env| {
+        let source = IteratorSource::new(0..10u32);
+        let res = env
+            .stream(source)
+            .group_by(|n| n % 2)
+            .map(|(_, x)| x.to_string())
+            .reduce(|acc, y| acc + &y)
+            .unkey()
+            .collect_vec();
+        env.execute();
+        if let Some(res) = res.get() {
+            let res = res.into_iter().sorted().collect_vec();
+            assert_eq!(res.len(), 2);
+            assert_eq!(res[0], (0, "02468".into()));
+            assert_eq!(res[1], (1, "13579".into()));
+        }
+    });
+}
