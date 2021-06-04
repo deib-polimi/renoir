24c569dd9f089311cccdf7900fd5fe3193e32a2f
Marco Donadoni <marcodonadoni@live.it>
2021-06-04 15:27:33 +0200

    Add pagerank example

---
+++ examples/pagerank.rs
@@ -0,0 +1,106 @@
+use std::time::Instant;
+
+use rstream::config::EnvironmentConfig;
+use rstream::environment::StreamEnvironment;
+use rstream::operator::source;
+
+const EPS: f64 = 1e-4;
+const DAMPENING: f64 = 0.85;
+
+fn main() {
+    let (config, args) = EnvironmentConfig::from_args();
+    if args.len() != 4 {
+        panic!("Pass the number of iterations, number of pages, pages dataset and links dataset as arguments");
+    }
+    let num_iterations: usize = args[0].parse().expect("Invalid number of iterations");
+    let num_pages: usize = args[1].parse().expect("Invalid number of pages");
+    let path_pages = &args[2];
+    let path_links = &args[3];
+
+    let mut env = StreamEnvironment::new(config);
+
+    env.spawn_remote_workers();
+
+    let pages_source = source::CsvSource::<u64>::new(path_pages).has_headers(false);
+    let links_source = source::CsvSource::<(u64, u64)>::new(path_links)
+        .delimiter(b' ')
+        .has_headers(false);
+
+    let adj_list = env
+        .stream(links_source)
+        // construct adjacency list
+        .group_by_fold(
+            |(x, _y)| *x,
+            Vec::new(),
+            |edges, (_x, y)| edges.push(y),
+            |edges1, mut edges2| edges1.append(&mut edges2),
+        )
+        .unkey();
+
+    let result = env
+        .stream(pages_source)
+        // distribute the ranks evenly
+        .map(move |x| (x, 1.0 / num_pages as f64, 1.0 / num_pages as f64))
+        .iterate(
+            num_iterations,
+            // state maintains whether a new iteration is needed
+            false,
+            move |s, _| {
+                let mut splits = s.split(2);
+                // keep ranks before being updated
+                let old_ranks = splits.pop().unwrap().map(|(x, _, rank)| (x, rank));
+
+                splits
+                    .pop()
+                    .unwrap()
+                    .map(|(x, _, rank)| (x, rank))
+                    .join(adj_list, |(x, _rank)| *x, |(x, _adj)| *x)
+                    .flat_map(|(_, ((_x, rank), (_, adj)))| {
+                        // distribuite the rank of the page between the connected pages
+                        let rank_to_distribute = rank / adj.len() as f64;
+                        adj.into_iter().map(move |y| (y, rank_to_distribute))
+                    })
+                    .unkey()
+                    .map(|(_x, (y, rank_to_distribute))| (y, rank_to_distribute))
+                    .group_by_fold(
+                        |(y, _)| *y,
+                        0.0,
+                        |rank, (_y, rank_to_distribute)| *rank += rank_to_distribute,
+                        |rank1, rank2| *rank1 += rank2,
+                    )
+                    // apply dampening factor
+                    .map(move |(_y, rank)| rank * DAMPENING + (1.0 - DAMPENING) / num_pages as f64)
+                    .unkey()
+                    // generate the pair of old and new rank for each page
+                    .join(old_ranks, |(y, _)| *y, |(y, _)| *y)
+                    .map(|(_, ((_, new), (_, old)))| (old, new))
+                    .unkey()
+                    .map(|(y, (old, new))| (y, old, new))
+            },
+            // a new iteration is needed if at least one page's rank has changed
+            |changed: &mut bool, (_x, old, new)| *changed = *changed || (new - old).abs() > EPS,
+            |state, changed| *state = *state || changed,
+            |state| {
+                let condition = *state;
+                *state = false;
+                condition
+            },
+        )
+        // keep the stream output
+        .1
+        .collect_vec();
+
+    let start = Instant::now();
+    env.execute();
+    let elapsed = start.elapsed();
+
+    if let Some(res) = result.get() {
+        eprintln!("Output: {:?}", res.len());
+        if cfg!(debug) {
+            for (x, _, rank) in res {
+                eprintln!("{}: {}", x, rank);
+            }
+        }
+    }
+    eprintln!("Elapsed: {:?}", elapsed);
+}
