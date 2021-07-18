2fc83f52da1ac46a18da9d53f862ee456e52aa74
Marco Donadoni <marcodonadoni@live.it>
2021-07-18 10:26:04 +0200

    Add documentation for processing-time windows

--- src/operator/window/description/mod.rs
+++ src/operator/window/description/mod.rs
@@ -13,17 +13,102 @@ mod count_window;
 mod session_window;
 mod sliding_window;
 
+/// Processing-time windows divide elements into windows based on their arrival time.
+/// The elements' timestamps are assigned automatically using the system time of the machine.
+/// For this reason, processing-time windows only support non-timestamped elements and cannot be
+/// used on streams of elements with timestamps and watermarks.
+///
+/// There are three different kinds of processing-time windows:
+///  - [`ProcessingTimeWindow::sliding`]
+///  - [`ProcessingTimeWindow::tumbling`]
+///  - [`ProcessingTimeWindow::session`]
+///
+/// Windows have an inclusive start timestamp and an exclusive end timestamp.
 pub struct ProcessingTimeWindow {}
 
 impl ProcessingTimeWindow {
+    /// Processing-time sliding windows have a fixed size and they may overlap.
+    /// The first window starts at timestamp zero, the second one at timestamp `step`, the third
+    /// one at timestamp `2 * step` and so on.
+    ///
+    /// This is equivalent to say that each window has `size` duration and starts `step` amount of
+    /// time after the beginning of the previous one.
+    ///
+    /// ## Example
+    /// ```
+    /// # use rstream::{StreamEnvironment, EnvironmentConfig};
+    /// # use rstream::operator::source::IteratorSource;
+    /// # use rstream::operator::window::ProcessingTimeWindow;
+    /// # use std::time::Duration;
+    /// # use itertools::Itertools;
+    /// # let mut env = StreamEnvironment::new(EnvironmentConfig::local(1));
+    /// let s = env.stream(IteratorSource::new((0..5)));
+    /// let res = s
+    ///     .window_all(ProcessingTimeWindow::sliding(Duration::from_secs(2), Duration::from_secs(1)))
+    ///     .map(|window| window.cloned().collect_vec())
+    ///     .collect_vec();
+    ///
+    /// env.execute();
+    /// let mut res = res.get().unwrap();
+    /// ```
     pub fn sliding(size: Duration, step: Duration) -> SlidingProcessingTimeWindowDescr {
         SlidingProcessingTimeWindowDescr::new(size, step)
     }
 
+    /// Processing-time tumbling windows have a fixed size and do not overlap.
+    /// They are equivalent to processing-time sliding windows with `step` equal to the size of the
+    /// window.
+    /// Because of this, each element is assigned to exactly one window.
+    ///
+    /// ## Example
+    /// In this example, tumbling windows with size of two seconds are used.
+    ///
+    /// The first window starts at timestamp zero (included) and ends at timestamp `2s` (excluded).
+    /// The second windows starts at timestamp `2s` and ends at timestamp `4s`, and so on.
+    /// ```
+    /// # use rstream::{StreamEnvironment, EnvironmentConfig};
+    /// # use rstream::operator::source::IteratorSource;
+    /// # use rstream::operator::window::ProcessingTimeWindow;
+    /// # use std::time::Duration;
+    /// # use itertools::Itertools;
+    /// # let mut env = StreamEnvironment::new(EnvironmentConfig::local(1));
+    /// let s = env.stream(IteratorSource::new((0..7)));
+    /// let res = s
+    ///     .window_all(ProcessingTimeWindow::tumbling(Duration::from_secs(2)))
+    ///     .map(|window| window.cloned().collect_vec())
+    ///     .collect_vec();
+    ///
+    /// env.execute();
+    /// let mut res = res.get().unwrap();
+    /// ```
     pub fn tumbling(size: Duration) -> SlidingProcessingTimeWindowDescr {
         SlidingProcessingTimeWindowDescr::new(size, size)
     }
 
+    /// Processing-time session windows do not have a fixed size and do not overlap.
+    ///
+    /// If there is no window open and a new element arrives, then a new window is created.
+    /// Otherwise, the element is added to the currently open window.
+    ///
+    /// When new elements do not arrive for `gap` amount of time, the currently open window is closed.
+    ///
+    /// ## Example
+    /// ```
+    /// # use rstream::{StreamEnvironment, EnvironmentConfig};
+    /// # use rstream::operator::source::IteratorSource;
+    /// # use rstream::operator::window::ProcessingTimeWindow;
+    /// # use std::time::Duration;
+    /// # use itertools::Itertools;
+    /// # let mut env = StreamEnvironment::new(EnvironmentConfig::local(1));
+    /// let s = env.stream(IteratorSource::new(vec![0, 1, 4, 5, 6, 9].into_iter()));
+    /// let res = s
+    ///     .window_all(ProcessingTimeWindow::session(Duration::from_secs(2)))
+    ///     .map(|window| window.cloned().collect_vec())
+    ///     .collect_vec();
+    ///
+    /// env.execute();
+    /// let mut res = res.get().unwrap();
+    /// ```
     pub fn session(gap: Duration) -> SessionProcessingTimeWindowDescr {
         SessionProcessingTimeWindowDescr::new(gap)
     }
