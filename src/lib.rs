7c7e1e5801e8dae11fb9de6322fd9a0e664d0273
imDema <luca.de.martini@protonmail.com>
2023-01-19 12:02:38 +0100

    Disable example doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 //! # Noir
-//! 
+//!
 //! ### Network of Operators In Rust
-//! 
+//!
 //! Noir is a distributed data processing platform based on the dataflow paradigm that provides an ergonomic programming interface, similar to that of Apache Flink, but has much better performance characteristics.
-//! 
-//! 
+//!
+//!
 //! Noir converts each job into a dataflow graph of
 //! operators and groups them in blocks. Blocks contain a sequence of operors which process the data sequentially without repartitioning it. They are the deployment unit used by the system and can be distributed and executed on multiple systems.
-//! 
+//!
 //! The common layout of a Noir program starts with the creation of a `StreamEnvironment`, then one or more `Source`s are initialised creating a `Stream`. The graph of operators is composed using the methods of the `Stream` object, which follow a similar approach to Rust's `Iterator` trait allowing ergonomically define a processing workflow through method chaining.
-//! 
+//!
 //! ### Example
-//! 
-//! ```
+//!
+//! ```no_run
 //! use noir::prelude::*;
-//! 
+//!
 //! fn main() {
 //!     // Convenience method to parse deployment config from CLI arguments
 //!     let (config, args) = EnvironmentConfig::from_args();
@@ -38,12 +38,12 @@
 //!         println!("{:?}", result);
 //!     }
 //! }
-//! 
+//!
 //! fn tokenize(s: &str) -> Vec<String> {
 //!     // Simple tokenisation strategy
 //!     s.split_whitespace().map(str::to_lowercase).collect()
 //! }
-//! 
+//!
 //! // Execute on 6 local hosts `cargo run -- -l 6 input.txt`
 //! ```
 
